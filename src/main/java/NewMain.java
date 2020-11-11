a0f207a6694b46ab4f769c7ed0fe27222a499364
niamh <niamh@DESKTOP-QC1ISBN>
2020-11-11 15:34:22 +0000

    Initial Commit

--- src/main/java/NewMain.java
+++ src/main/java/NewMain.java
@@ -15,7 +15,7 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World, my name is Niamh.");
         // TODO code application logic here
     }
     
